11bae7f2eb07c14b62734036eaa33e10327df15d
Gabriel Queiroz <gqmv@cin.ufpe.br>
2022-03-31 16:43:56 -0300

    Added testing for PCSrcCtrlMux

--- parts-made/PCSrcCtrlMux.v
+++ parts-made/PCSrcCtrlMux.v
@@ -1,9 +1,10 @@
-module mux_pc_src_ctrl (
+module PCSrcCtrlMux (
     input   wire  [1:0]    PCSourceCtrl,
     input   wire  [31:0]   Exception_Destiny,   // 00
     input   wire  [31:0]   EPC_Out,             // 01
     input   wire  [31:0]   ALU_Out,             // 10
     input   wire  [31:0]   Shift_Left_2,        // 11
+
     output   wire  [31:0]  Data_out
 );
 
@@ -17,8 +18,8 @@ module mux_pc_src_ctrl (
     wire [31:0] EPC_EXP;
     wire [31:0] Shift_ALU;
 
-    assign EPC_EXP = (PCSourceCtrl[0]) ? EPC_Out : Exception_Destiny
-    assign Shift_ALU = (PCSourceCtrl[0]) ? Shift_Left_2 : ALU_Out
-    assign Data_out = (PCSourceCtrl[1]) ? Shift_ALU : EPC_EXP
+    assign EPC_EXP = (PCSourceCtrl[0]) ? EPC_Out : Exception_Destiny;
+    assign Shift_ALU = (PCSourceCtrl[0]) ? Shift_Left_2 : ALU_Out;
+    assign Data_out = (PCSourceCtrl[1]) ? Shift_ALU : EPC_EXP;
 
 endmodule
