56ece67bcbd83491ed9e3cdea4564d6549d7cf8b
Gabriel Queiroz <gqmv@cin.ufpe.br>
2022-04-01 12:25:44 -0300

    Added control unit
    Co-authored-by: Clesson Roberto da Silva Júnior <ClessonR@users.noreply.github.com>

--- developed-components/mux/WriteRegMux.v
+++ developed-components/mux/WriteRegMux.v
@@ -1,6 +1,6 @@
 module WriteRegMux(
     input  wire [1:0]  WriteRegCtrl,
-    input  wire [4:0] Instruction_15_11,
+    input  wire [15:0] Instruction_15_0,
     input  wire [4:0] Instruction_20_16,
 
     output wire [4:0] Data_out
@@ -13,6 +13,8 @@ module WriteRegMux(
 //                            | ---
 // 31 ----------------- | --- |
 // Instruction_20_16 -- |
+    wire [4:0] Instruction_15_11;
+    assign Instruction_15_11 = Instruction_15_0[15:11];
 
     wire [4:0] int_Instruction_15_11_29;
     wire [4:0] int_31_Instruction_20_16;
