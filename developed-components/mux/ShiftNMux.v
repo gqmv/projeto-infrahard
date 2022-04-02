4bbddb152e5c7c5a8215a1329564045b4c090359
Gabriel Queiroz <gqmv@cin.ufpe.br>
2022-04-02 15:27:04 -0300

    Added testing

--- developed-components/mux/ShiftNMux.v
+++ developed-components/mux/ShiftNMux.v
@@ -4,7 +4,7 @@ module ShiftNMux(
     input  wire [31:0] B,                //00
     input  wire [31:0] MemDataOut,       //01
     input  wire [15:0] Instruction_15_0, //10
-    output wire [31:0] Data_out
+    output wire [4:0] Data_out
 
 );
 
@@ -13,14 +13,9 @@ module ShiftNMux(
 //                                   |---Data_out --->
 // Instruction_10_6-----------------/
 
+    wire [4:0] B_MemOut;
 
-
-    wire [4:0] Instruction_10_6;
-    assign Instruction_10_6 = Instruction_10_6[10:6];
-
-    wire [31:0] B_MemOut
-
-    assign B_MemOut = (ShiftNCrtl[0]) ? MemDataOut: B;
-    assign Data_out = (ShiftNCrtl[1]) ? Instruction_10_6 : B_MemOut;
+    assign B_MemOut = (ShiftNCrtl[0]) ? MemDataOut[4:0] : B[4:0];
+    assign Data_out = (ShiftNCrtl[1]) ? Instruction_15_0[10:6] : B_MemOut;
 
 endmodule
